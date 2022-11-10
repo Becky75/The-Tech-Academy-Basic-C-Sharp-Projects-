c3cac8cbde26f275bbd76a2c27777d604dfcf3eb
Becky75 <alpaca.love5@gmail.com>
2022-11-10 12:34:33 -0800

    update

---
+++ test/test/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int total  = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus Five = " + difference.ToString());
+            //Console.ReadLine();
+
+
+            //int Quotient = 100 / 5;
+            //Console.WriteLine(Quotient);
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 < 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //int roomTemperature = 70;
+            //int currentTemperature = 70;
+            ////bool isWarm = currentTemperature <= roomTemperature;
+            //bool isWarm = currentTemperature == roomTemperature;
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            //take input from user, multiples it by 50,then prints result
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = 50;
+            int answer = num1 * num2;
+            Console.WriteLine(answer);
+            Console.ReadLine();
+
+            ////// take input frm user, adds 25 to it
+
+            //int num3 = Convert.ToInt32(Console.ReadLine());
+            //int num4 = 25;
+            //int addition = num1 + num2;
+            //Console.WriteLine(addition);
+            //Console.ReadLine();
+
+
+            ////this is a divison 
+            ////int division = () / 7;
+
+            //int num5 = Convert.ToInt32(Console.ReadLine());
+            //int num6 = 7;
+            //int division = num5 / num6;
+            //Console.WriteLine(division);
+            //Console.ReadLine();
+
+
+
+
+        }
+    }
+}
