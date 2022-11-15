698af5a7ff17a24cf08bce107b565c376cce167e
Becky75 <alpaca.love5@gmail.com>
2022-11-14 19:58:23 -0800

    update

--- BoolLogicAssignment/Program.cs
+++ BoolLogicAssignment/Program.cs
@@ -6,7 +6,16 @@ namespace BoolLogicAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+         //these are the guessing to help decide the end result
+            Console.WriteLine("Whats your age?");
+            int age = (age) > 15;
+            Console.WriteLine("Have you ever had a DUI?");
+            bool dui=(true && true);
+            Console.WriteLine("How many speeding tickets do you have?");
+
+            Console.WriteLine("Qualified?")
+            bool Quualified = (ticket > 3 && age < 15); false);
         }
     }
 }
