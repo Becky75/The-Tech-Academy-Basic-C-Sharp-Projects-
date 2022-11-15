b3a555b6a006d96617e93ebf38eb75b2bfcf3bdb
Becky75 <alpaca.love5@gmail.com>
2022-11-15 15:40:22 -0800

    updated

--- BoolLogicAssignment/Program.cs
+++ BoolLogicAssignment/Program.cs
@@ -9,23 +9,23 @@ namespace BoolLogicAssignment
 
          //these are the guessing to help decide the end result
             Console.WriteLine("Whats your age?");
-            int age = 16;
-            bool oldEnough = (age > 15);
+            int age = Convert.ToInt32(Console.ReadLine());
+           
 
             //this a trueOrFalse question.
 
             Console.WriteLine("Have you ever had a DUI?");
-            bool dui=(true==false);
+            bool dui =Convert.ToBoolean(Console.ReadLine());
 
             //this is a greater then comparision 'do they have over 3 tickets' 
 
             Console.WriteLine("How many speeding tickets do you have?");
-            int ticket = 3;
+            int ticket = Convert.ToInt32(Console.ReadLine());
 
             //this is a bool "do they Qualifie?
-            Console.WriteLine("Qualified?");
-            bool Qualified = (ticket > 3 && age > 15);
-            Console.WriteLine("You Qualify!");
+            Console.WriteLine("Qualified!");
+            bool Qualified = (ticket <= 3 && age > 15 && !dui);
+           
             
         }
     }
