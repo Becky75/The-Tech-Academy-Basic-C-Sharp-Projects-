c9e4d39fd2cb6e885fa8c01665d723c8efa26916
Becky75 <alpaca.love5@gmail.com>
2022-11-29 11:38:10 -0800

    updated part 3

--- sixpartassignment2/Program.cs
+++ sixpartassignment2/Program.cs
@@ -7,6 +7,7 @@ namespace sixpartassignment2
     class Program
     {
         static void Main(string[] args)
+            //this is part 3, comparision < loop operator
         {
             string[] horses = { "Arab", "Mustang", "Paint", "Gypsies" };
             for (int i = 0; i < horses.Length ; i++)
@@ -14,10 +15,12 @@ namespace sixpartassignment2
                 Console.WriteLine(horses[i]);
             }
 
+            //this is second part comparision <=loop operator
             for (int b=0; b<=10; b=b +2)
             {
                 Console.WriteLine(b);
             }
+
             Console.ReadLine();
         }
     }
