4ca9fafb5808d35c89d0a87add70718d8e750bc4
Becky75 <alpaca.love5@gmail.com>
2022-11-30 14:44:39 -0800

    part4 updated

--- sixpartassignment2/Program.cs
+++ sixpartassignment2/Program.cs
@@ -7,7 +7,7 @@ namespace sixpartassignment2
     class Program
     {
         static void Main(string[] args)
-            //this is part 3, comparision < loop operator
+    //this is part 3, comparision < loop operator
         {
             string[] horses = { "Arab", "Mustang", "Paint", "Gypsies" };
            
@@ -16,12 +16,16 @@ namespace sixpartassignment2
                 Console.WriteLine(horses[i]);
             }
 
-            //this is second part comparision <= loop operator
+    //this is second part comparision <= loop operator
             for (int b=0; b<=10; b=b +2)
             {
                 Console.WriteLine(b);
             }
-
+            //part4 each item in the list is unque.
+            {
+                int[] num = { 6, 3, 4, 8, 67, 12 };
+                Console.WriteLine(num[]);
+            }
             Console.ReadLine();
         }
     }
---
+++ sixpartassignment3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace sixpartassignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is part 4,a unqigue values
+            List<string> homes = new List<string>()
+            {
+                "Rancher","Farm","Condo","Appartment","Two story","Barn"
+            };
+            {
+                Console.WriteLine(homes);
+                    }
+            Console.ReadLine();
+        }
+    }
+}
