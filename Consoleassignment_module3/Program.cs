9a6bb8df6ff9c817bc3747fb7a3bbc3c8dd3b819
Becky75 <alpaca.love5@gmail.com>
2022-12-07 14:29:16 -0800

    updated

--- Consoleassignment_module3/Program.cs
+++ Consoleassignment_module3/Program.cs
@@ -7,14 +7,17 @@ namespace Consoleassignment_module3
     {
         static void Main(string[] args)
         {
-           
+                //Console.WriteLine(8, 16, 24);
+                //Console.WriteLine(Convert.ToInt32)
             try
-            {
+            { 
+                int[] num1 = { 8, 4, 9 };
 
+                for (int i= 0; i < num1.Length; i++)
+                {
                 Console.WriteLine("choose a numbers 8,4,9");
                 int num1 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("pick anouther number");
-                int num2 = Convert.ToInt32(Console.ReadLine());
+                int num2 = Convert.ToInt32(Console.ReadLine(2)); 
                 Console.WriteLine("now to divide the two....");
                 int num3 = num1 / num2;
                 Console.WriteLine(num1 + " divide by " + num2 + " equals ");
