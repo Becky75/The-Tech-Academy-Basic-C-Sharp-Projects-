2fa88be633a22ba329ea98bbfa506e69125617b4
Becky75 <alpaca.love5@gmail.com>
2022-12-07 10:43:30 -0800

    updated

---
+++ Consoleassignment_module3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace Consoleassignment_module3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            try
+            {
+
+                Console.WriteLine("choose a numbers 8,4,9");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("pick anouther number");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("now to divide the two....");
+                int num3 = num1 / num2;
+                Console.WriteLine(num1 + " divide by " + num2 + " equals ");
+                Console.ReadLine();
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("please dont divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+         
+
+          
+                
+        }
+    }
+}
