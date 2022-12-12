833ca8b8138754d79ebbe70b15bdb2d8437f877c
Becky75 <alpaca.love5@gmail.com>
2022-12-12 14:34:06 -0800

    updated

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Class1
+    {
+        // 
+
+        public int user(int i)
+        {
+            // a method that takes two integers as parameters.Make one of them optional. Have the method do a math operation and return an integer result.
+
+            return i;
+        }
+        //secound number 
+        public int user2(int k)
+        {
+            return  k;
+            }
+        public int Add1(int j) 
+        {
+            return user + user2;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // In the Main() method of the console app, instantiate the class.
+
+
+            //Ask the user to input two numbers, one at a time.Let them know they need not enter anything for the second number.
+
+            //Call the method in the class, passing in the one or two numbers entered.
+
+            Class1 obj1 = new Class1();
+            Console.WriteLine("pick two numbers, one at a time First number is?");
+            int user = Convert.ToInt32(Console.ReadLine());
+
+            Class1 obj2 = new Class1();
+            Console.WriteLine(" now pick a second number");
+            int user2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(obj1.user(user) + " + " + obj2.user2(user2) + "="+ Add1.();
+
+            Console.ReadLine();
+
+        }
+       
+
+
+        //Try various combinations of numbers on the code, including having no second number.
+
+
+}
+}
