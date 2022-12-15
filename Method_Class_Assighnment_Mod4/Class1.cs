512a2e00ff201636bd5d7852e22be201152aebd3
Becky75 <alpaca.love5@gmail.com>
2022-12-15 11:49:36 -0800

    just the start

--- Method_Class_Assighnment_Mod4/Class1.cs
+++ Method_Class_Assighnment_Mod4/Class1.cs
@@ -7,6 +7,24 @@ using System.Threading.Tasks;
 namespace Method_Class_Assighnment_Mod4
 {
     class Class1
+    // this is a void method that takes two integers as parameters.Have the method do a math operation on the first integer and display the second integer to the screen.
+
+        public int Add1(int i)
     {
+        //code to be executed
+
+        return i + 3;
+    }
+    public int num1(int j)
+    {
+        int k = Convert.ToInt32(int j);
+        return k;
+    }
+
+        //int num2 = 2;
+             
+        
+        
+      
     }
 }
--- Method_Class_Assighnment_Mod4/Program.cs
+++ Method_Class_Assighnment_Mod4/Program.cs
@@ -8,8 +8,19 @@ namespace Method_Class_Assighnment_Mod4
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(int[] args)
         {
+            //  create a void method that takes two integers as parameters.Have the method do a math operation on the first integer and display the second integer to the screen.
+
+            //In the Main() method of the console app, instantiate the class.
+            Class1 class1 = new Class1();
+            int i = 4;
+
+//Call the method in the class, passing in two numbers.
+            
+            //Call the method in the class, specifying the parameters by name.
+            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
