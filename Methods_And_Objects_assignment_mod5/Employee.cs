da0380273de4cb55f12365f810a7613dc83b03cb
Becky75 <alpaca.love5@gmail.com>
2022-12-27 11:22:27 -0800

    updated

--- Methods_And_Objects_assignment_mod5/Employee.cs
+++ Methods_And_Objects_assignment_mod5/Employee.cs
@@ -8,27 +8,40 @@ namespace Methods_And_Objects_assignment_mod5
 {
     //Create another class called Employee and have it inherit from the Person class. Give the Employee class a property called Id and have it be of data type int.
     // interface file added to the inherated class  
-    public class Employee : IQuittable 
+    public class Employee : IQuittable
     {
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         //Bool comparison operator overload 
-        public bool operator == (Employee emp1, Employee emp2)
+        //static method
+        public static bool ();
+
+
+        public bool operator ==(Employee emp1, Employee emp2)
         {
             if (emp1==emp2)
             {
-                Console.WriteLine("true");
-
+                return emp1;
+                Console.WriteLine();    
             }
+            else
+            {
+                Console.WriteLine("true");
+            }
+
         }
 
-        public bool operator != (Employee emp1, Employee emp2)
+        public bool operator !=(Employee emp1, Employee emp2)
         {
             if (emp1!=emp2)
             {
+                return emp2;
+                Console.WriteLine();
+            }
+            else
+            {
                 Console.WriteLine("false");
-
             }
 
         }
