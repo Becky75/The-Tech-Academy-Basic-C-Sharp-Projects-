6b4f9571587ccb7134c7160246ed503ff68850f1
Becky75 <alpaca.love5@gmail.com>
2023-01-02 14:50:12 -0800

    uploaded

---
+++ Parsing_Enums_assignment_mod6/Days.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums_assignment_mod6
+{
+    public class Days
+    {
+        public DayOfWeek Day { get; set; }
+    }
+    public enum Day
+    {
+        Monday,
+        Tuesday,
+        Wensday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ Parsing_Enums_assignment_mod6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums_assignment_mod6
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //this is a try/catch
+            try
+            { 
+            //this is a enum for the days of the week
+            DaysOfTheWeek Day = DaysOfTheWeek.Monday;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine("Please enter an actual day of the week.");
+            
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wensday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+     
+ }
+
