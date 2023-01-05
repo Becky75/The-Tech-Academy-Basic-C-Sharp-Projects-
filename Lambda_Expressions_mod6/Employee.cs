15c3dffc3e7884b5ffbd489b641e13fe1910a845
Becky75 <alpaca.love5@gmail.com>
2023-01-05 15:33:03 -0800

    updated

---
+++ Lambda_Expressions_mod6/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Expressions_mod6
+{
+   public class Employee
+    {
+
+        //Add a property called “things” and have its data type be a generic list matching the generic type of the class
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //Create an Employee class with the following properties: ID,First name, Last name 
+
+    }
+}
