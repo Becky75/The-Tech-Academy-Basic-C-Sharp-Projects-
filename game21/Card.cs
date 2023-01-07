422e40176e5e01925aee1a70de7298277c21cb4f
Becky75 <alpaca.love5@gmail.com>
2023-01-06 17:44:25 -0800

    updated

--- game21/Card.cs
+++ game21/Card.cs
@@ -11,7 +11,12 @@ namespace game21
         
         public Suit Suit { get; set; }
         public Face Face { get; set; }
-           
+
+        public override string ToString()
+        {
+            return string.Format("{0} of {1}", Face, Suit);
+        }
+
     }
     public enum Suit
     {
--- game21/Program.cs
+++ game21/Program.cs
@@ -8,8 +8,46 @@ namespace game21
     {
         static void Main(string[] args)
         {
-          
+            //prints welcome comment and can enter the player name
+            Console.WriteLine("welcome to the Grand Hotel and Casino. Lets start by telling me your name.");
+            //reads player name, 
+            string playerName = Console.ReadLine(); 
+            
+            // asks and enters amount of money           
+            Console.WriteLine("And how much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());            
+           
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            //this allows for different answers 
+            string answer = Console.ReadLine().ToLower();
+           
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
+            {
+                //this is a constructor
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                //adds player
+                game += player;
+                player.isActivelyPlaying = true;
+                //this keeps player playing as long as playing amd has a balance greater then zero
+                while (player.isActivelyPlaying && player.Balance >0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+            }
+            Console.WriteLine("Feel free to look around the casino, Bye for now");
 
+            Console.ReadLine();
+
+            
+            
+            
+            
+            
+            
+            
             //operator
             //Game game = new TwentyOneGame();
             //game.Players = new List<Player>();
@@ -18,23 +56,23 @@ namespace game21
             //game += player;
             //game -= player;
 
-            // an other way to write it- Deck deck = new Deck() { Face = "king", Suit = "spades" }; - does the same as below code
-            Deck deck = new Deck();
+            //// an other way to write it- Deck deck = new Deck() { Face = "king", Suit = "spades" }; - does the same as below code
+            //Deck deck = new Deck();
 
 
-            // //this is an emptey list 
-            deck.Shuffle(3);
-            ////this is how to shuffle x amount of times
+            //// //this is an emptey list 
+            //deck.Shuffle(3);
+            //////this is how to shuffle x amount of times
 
 
-            // // this is to have a suit and face to cards 
+            //// // this is to have a suit and face to cards 
 
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
-            Console.ReadLine();
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+            //Console.WriteLine(deck.Cards.Count);
+            //Console.ReadLine();
 
         }
         
